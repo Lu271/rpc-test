98b82a4400c67d07dc775e05af61bb00d930a5e2
lushilong <betta@bettadeMacBook-Pro-3.local>
2024-11-22 18:39:54 +0800

    [feat] 加个小log

--- hello-server/rpc/hello/handler.go
+++ hello-server/rpc/hello/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	hello "github.com/Lu271/rpc-test/hello-server/kitex_gen/hello"
+	"log"
 )
 
 // HelloServiceImpl implements the last service interface defined in the IDL.
@@ -11,6 +12,7 @@ type HelloServiceImpl struct{}
 
 // SayHello implements the HelloServiceImpl interface.
 func (s *HelloServiceImpl) SayHello(ctx context.Context, req *hello.HelloRequest) (resp *hello.HelloResponse, err error) {
+	log.Println("say hello in")
 	resp = &hello.HelloResponse{}
 	resp.Message = fmt.Sprintf("I am Hello server, I receive %v", req.Message)
 	return
